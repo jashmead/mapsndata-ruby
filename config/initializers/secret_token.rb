ef32cda4d8614f7a39582a8ecff60bf60492cd2a
jashmead <john.ashmead@ashmeadsoftware.com>
2013-12-16 14:43:47 -0500

    initial setup of mapsndata; the result of "rails new mapsndata"

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mapsndata::Application.config.secret_key_base = '4dc91efb3339ef3e536b53b94b5383fd98f7c0576ba935b8b273e532f0de25b859429f56fdaa69e4b9992cba4459ee661acab3169f3f11f640c6cb57f52510c5'
